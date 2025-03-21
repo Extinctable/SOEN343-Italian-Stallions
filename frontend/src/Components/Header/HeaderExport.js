b54f9848762d297c21563ab2a8a1b83a080debe1
Extinctable <massimo02caruso@gmail.com>
2025-03-21 10:59:04 -0400

    Header component used to export the header and its elements to other pages.

---
+++ frontend/src/Components/Header/HeaderExport.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/styles.css";
+import navigation from "./data/data";
+import Logo from "../../stallion logo.webp";
+
+import HeaderMain from "./HeaderMain";
+
+export default function Main() {
+  return (
+    <div className="App">
+      <HeaderMain items={navigation} logo={<img src={Logo} alt="Logo" height={150} />} navPosition="center" />
+      
+    </div>
+  );
+}
